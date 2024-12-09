b8b9cbbd049eee3582a043d6b2ee2abc3dfee0a5
Cwqing9217 <154387130+Cwqing9217@users.noreply.github.com>
2024-12-09 06:12:37 -0500

    Create Feedback.php

---
+++ Feedback.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+    $email= $_POST['email'];
+    $feedback = $_POST['feedback'];
+    echo "Thank you!" . $name . "！We will contact you asap.";
+}
+?>
