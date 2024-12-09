ecc42aadb95eadc1f6fbdf66880d0bcaf2b2147e
Cwqing9217 <154387130+Cwqing9217@users.noreply.github.com>
2024-12-09 02:56:02 -0500

    Update FAQ_script.js

--- FAQ_script.js
+++ FAQ_script.js
@@ -1,1 +1,13 @@
-
+const Small = document.querySelectorAll('.FAQ_S');
+FAQ_S.forEach(item => {
+    const Question = item.querySelector('.question');
+    const Answer = item.querySelector('.answer');
+    question.addEventListener('click', () => {
+        Small.forEach(el => {
+            if (el !== item) {
+                el.querySelector('.answer').classList.remove('show');
+            }
+        });
+        Answer.classList.toggle('show');
+    });
+});
